00bb08cea33ea6d99a24f1d5d0d46b5ec369fac2
nicolk <117823281+nicolkrit999@users.noreply.github.com>
2023-06-01 23:23:46 +0200

    Project start

--- 0001 Developing projects/0000 Python/0000 Penguin_Shape (pytamaro library)/helper_functions.py
+++ 0001 Developing projects/0000 Python/0000 Penguin_Shape (pytamaro library)/helper_functions.py
@@ -1,7 +1,5 @@
 from pytamaro import *
-import main as main
+
 
 def prova() -> Graphic:
-    return beside(main.pytamaro_logo(25, blue, red), rectangle(100, 50, yellow))
-
-show_graphic(prova())
+    return None
--- 0001 Developing projects/0000 Python/0000 Penguin_Shape (pytamaro library)/main.py
+++ 0001 Developing projects/0000 Python/0000 Penguin_Shape (pytamaro library)/main.py
@@ -1,17 +1,4 @@
 from pytamaro import *
-
-def pytamaro_logo(side1, color1, color2) -> Graphic:
-    return compose(pin(bottom_right,
-        (compose(rotate(180, pin(top_center, triangle(side1, side1, 60, color1))),
-                 pin(bottom_left, triangle(side1 * 2, side1 * 2, 60, color2))))),
-         rotate(180, pin(top_center, triangle(side1, side1, 60, color1))))
+# import helper_functions as helper
 
 
-# show_graphic(pytamaro_logo(300, blue, red))
-
-
-def prova_beside(side) -> Graphic:
-    return beside(rectangle(side, side / 2, red), pytamaro_logo(side, blue, red))
-
-
-# show_graphic(prova_beside(100))
