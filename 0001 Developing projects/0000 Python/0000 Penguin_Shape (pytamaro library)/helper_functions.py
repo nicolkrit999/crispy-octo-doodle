3dc0e8fab8cd4759cf3d891cecc02002aa96ee67
nicolk <117823281+nicolkrit999@users.noreply.github.com>
2023-06-02 01:25:12 +0200

    helper functions (eye not complete)

--- 0001 Developing projects/0000 Python/0000 Penguin_Shape (pytamaro library)/helper_functions.py
+++ 0001 Developing projects/0000 Python/0000 Penguin_Shape (pytamaro library)/helper_functions.py
@@ -1,5 +1,44 @@
 from pytamaro import *
 
 
-def prova() -> Graphic:
-    return None
+# ratios may be changed in respect to one function to another
+# all ellipse helper functions are based on 1.20 Length/Width ratio
+# wings helper function is based on a 2.5 Length/Width ratio.
+
+# function variable may be changed only if the ratio is respected
+def main_black_body(short_axis) -> Graphic:
+    return ellipse(short_axis, short_axis * 1.2, black)
+
+
+# show_graphic(main_black_body(750))
+
+
+# function variable may be changed only if the ratio is respected
+def main_white_body(short_axis) -> Graphic:
+    return ellipse(short_axis, short_axis * 1.2, white)
+
+
+# show_graphic(main_white_body(100))
+
+
+# an equilateral triangle does not have a ratio to respect for its sides
+def feet(side_length) -> Graphic:
+    return triangle(side_length, side_length, 60, yellow)
+
+
+# show_graphic(feet(100))
+
+
+def wings(short_axis) -> Graphic:
+    return ellipse(short_axis, short_axis * 2.5, black)
+
+
+# show_graphic(wings(100))
+
+
+def eye(diameter) -> Graphic:
+    return compose(pin(bottom_center, ellipse(diameter * 0.5, diameter * 0.5, black)),
+                       pin(center, ellipse(diameter * 0.75, diameter * 0.75, white)))
+
+
+show_graphic(eye(500))
--- 0001 Developing projects/0000 Python/0000 Penguin_Shape (pytamaro library)/main.py
+++ 0001 Developing projects/0000 Python/0000 Penguin_Shape (pytamaro library)/main.py
@@ -1,4 +1,10 @@
 from pytamaro import *
-# import helper_functions as helper
+import helper_functions as helper  # defines how the helper_functions.py file is called here
 
 
+# The short axis of the main black body defines the penguin dimension
+# Changing this variable may result in the need to change most of the helper_functions ratios in the return statement
+def penguin_shape(short_axis) -> Graphic:
+    return overlay(helper.main_white_body(short_axis * 0.75), helper.main_black_body(short_axis))
+
+# show_graphic(penguin_shape(1000))
