c8ff365c1382a25808a8fa2b21945e7fec0c61d4
nicolk <117823281+nicolkrit999@users.noreply.github.com>
2023-06-02 11:25:38 +0200

    cose strane

--- 0001 Developing projects/0000 Python/0000 Penguin_Shape (pytamaro library)/helper_functions.py
+++ 0001 Developing projects/0000 Python/0000 Penguin_Shape (pytamaro library)/helper_functions.py
@@ -6,19 +6,19 @@ from pytamaro import *
 # wings helper function is based on a 2.5 Length/Width ratio.
 
 # function variable may be changed only if the ratio is respected
-def main_black_body(short_axis) -> Graphic:
+def main_black_oval(short_axis) -> Graphic:
     return ellipse(short_axis, short_axis * 1.2, black)
 
 
-# show_graphic(main_black_body(750))
+# show_graphic(main_black_oval(750))
 
 
 # function variable may be changed only if the ratio is respected
-def main_white_body(short_axis) -> Graphic:
+def main_white_oval(short_axis) -> Graphic:
     return ellipse(short_axis, short_axis * 1.2, white)
 
 
-# show_graphic(main_white_body(100))
+# show_graphic(main_white_oval(100))
 
 
 # an equilateral triangle does not have a ratio to respect for its sides
@@ -36,9 +36,68 @@ def wings(short_axis) -> Graphic:
 # show_graphic(wings(100))
 
 
+def left_half_eye(diameter) -> Graphic:
+    return rotate(90, compose(pin
+                              (bottom_right, circular_sector(diameter / 2.25, 180, black)),
+                              circular_sector(diameter, 180, white)))
+
+
+# show_graphic(left_half_eye(500))
+
+
+def right_half_eye(diameter) -> Graphic:
+    return rotate(-90, compose(pin(bottom_left,
+                                   circular_sector(diameter / 2.25, 180, black)),
+                               circular_sector(diameter, 180, white)))
+
+
+# show_graphic(right_half_eye(500))
+
+
 def eye(diameter) -> Graphic:
-    return compose(pin(bottom_center, ellipse(diameter * 0.5, diameter * 0.5, black)),
-                       pin(center, ellipse(diameter * 0.75, diameter * 0.75, white)))
+    return beside(left_half_eye(diameter), right_half_eye(diameter))
 
 
-show_graphic(eye(500))
+# show_graphic(eye(500))
+
+
+def penguin_top_left(diameter) -> Graphic:
+    return beside(compose(pin(bottom_right, eye(diameter / 3)),
+                   pin(bottom_right, rotate(90, circular_sector(diameter, 90, black)))), rectangle(diameter / 2, diameter, black), )
+
+
+# show_graphic(penguin_top_left(500))
+
+
+def penguin_top_right(diameter) -> Graphic:
+    return beside(rectangle(diameter / 2, diameter, black),
+                  compose(pin(bottom_left, eye(diameter / 3)), pin(bottom_left, circular_sector(diameter, 90, black))))
+
+
+# show_graphic(penguin_top_right(500))
+
+
+def penguin_top_portion(diameter) -> Graphic:
+    return overlay(beside(penguin_top_left(diameter / 2), penguin_top_right(diameter / 2)), circular_sector(diameter, 180, black))
+
+
+show_graphic(penguin_top_portion(500))
+
+
+
+
+"""
+def penguin_black_body_no_wings(diameter) -> Graphic:
+    return compose(pin(top_center, penguin_top_portion(diameter)),
+            pin(top_center, main_black_oval(diameter * 2)))
+
+
+# show_graphic(penguin_black_body_no_wings(500))
+
+
+def penguin_black_body_with_wings(diameter) -> Graphic:
+    return compose(pin(center, wings(diameter / 2) ), pin(center_left, penguin_black_body_no_wings(diameter)) )
+
+
+# show_graphic(penguin_black_body_with_wings(500))
+"""
--- 0001 Developing projects/0000 Python/0000 Penguin_Shape (pytamaro library)/main.py
+++ 0001 Developing projects/0000 Python/0000 Penguin_Shape (pytamaro library)/main.py
@@ -5,6 +5,6 @@ import helper_functions as helper  # defines how the helper_functions.py file is
 # The short axis of the main black body defines the penguin dimension
 # Changing this variable may result in the need to change most of the helper_functions ratios in the return statement
 def penguin_shape(short_axis) -> Graphic:
-    return overlay(helper.main_white_body(short_axis * 0.75), helper.main_black_body(short_axis))
+    return empty_graphic()
 
 # show_graphic(penguin_shape(1000))
