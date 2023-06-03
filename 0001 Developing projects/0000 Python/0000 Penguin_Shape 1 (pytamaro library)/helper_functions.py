7d2f4ba51a8916a72f2119e82329b4928bdf5283
nicolk <117823281+nicolkrit999@users.noreply.github.com>
2023-06-03 14:27:24 +0200

    various

--- 0001 Developing projects/0000 Python/0000 Penguin_Shape 1 (pytamaro library)/helper_functions.py
+++ 0001 Developing projects/0000 Python/0000 Penguin_Shape 1 (pytamaro library)/helper_functions.py
@@ -5,6 +5,7 @@ orange = rgb_color(255, 165, 0)  # rgb color definition for the feet and beak
 
 # ratios may be changed in respect to one function to another
 # function variable may be changed only if the function body is changed accordingly
+# the dimensions do not respect a ratio however they scale well for any variable input value
 
 
 def left_eye(radius) -> Graphic:
@@ -16,27 +17,28 @@ def left_eye(radius) -> Graphic:
 # show_graphic(left_eye(500))
 
 
-def right_eye(radius) -> Graphic:
-    return rotate(180, compose(pin(center_right,
-                                   overlay(ellipse(radius / 2, radius / 2, black), ellipse(radius, radius, white))),
-                               pin(center_left,
-                                   rectangle(radius / 2, radius, black))))  # rectangle allow the pupils to not touch
+def right_eye(radius) -> Graphic:  # using the left_eye allow the eyes do be modified at the same time
+    return rotate(180,
+                  left_eye(radius))
 
 
 # show_graphic(right_eye(500))
 
 
-def beak(side_length) -> Graphic:
-    return compose(pin(top_center, rotate(180, triangle(side_length, side_length * 2, 75.522, orange))),
+def beak_belly(side_length) -> Graphic:
+    return compose(pin(top_center,
+                       rotate(180,  # rotating the beak now allow to not shift the center pin in other functions
+                              triangle(side_length, side_length * 2, 75.522, orange))),  # the angle was computed
+                   # with an online tool
                    pin(top_center, ellipse(side_length * 4, side_length * 5.5, white)))
 
 
-# show_graphic(beak(100))
+# show_graphic(beak_belly(100))
 
 
-def eyes_beak_belly(radius) -> Graphic:
+def eyes_beak_belly(radius) -> Graphic:  # putting together these 3 elements allows to have the belly as center element
     return compose(pin(bottom_center, beside(left_eye(radius / 2), right_eye(radius / 2))),
-                   pin(top_center, beak(radius / 2.5)))
+                   pin(top_center, beak_belly(radius / 2.5)))
 
 
 # show_graphic(eyes_beak_belly(500))
@@ -45,7 +47,8 @@ def eyes_beak_belly(radius) -> Graphic:
 # show_graphic(left_feet(100))
 
 
-def right_foot(side_length) -> Graphic:
+def right_foot(side_length) -> Graphic:  # the circular_sector is hidden by the belly,
+    # however it is necessary to not have the feet touch each other in the final penguin shape
     return compose(pin(top_left, rotate(45, triangle(side_length / 2, side_length / 2, 60, orange))),
                    pin(bottom_right, rotate(-90, circular_sector(side_length / 2, 90, white))))
 
@@ -53,32 +56,32 @@ def right_foot(side_length) -> Graphic:
 # show_graphic(right_foot(500))
 
 
-def left_foot(side_length) -> Graphic:
+def left_foot(side_length) -> Graphic:  # using the right_foot allow the feet do be modified at the same time
     return rotate(-90, right_foot(side_length))
 
 
 # show_graphic(left_foot(500))
 
 
-def feet(side_length) -> Graphic:
+def feet(side_length) -> Graphic:  # necessary to not shift the bottom_center pin of the belly to the center of the foot
     return beside(left_foot(side_length), right_foot(side_length))
 
 
 # show_graphic(feet(500))
 
 
-def wing(short_axis) -> Graphic:
+def wing(short_axis) -> Graphic:  # the height may be changed without problems
     return ellipse(short_axis, short_axis * 2.5, black)
 
 
-def beak_feet_belly_eyes(side) -> Graphic:
+def beak_feet_belly_eyes(side) -> Graphic:  # allow to have the center of the belly to easily create the body
     return compose(pin(bottom_center, eyes_beak_belly(side / 1.2)),
                    pin(bottom_center, feet(side)))
 
 # show_graphic(beak_feet_belly_eyes(100))
 
 
-def body(size) -> Graphic:
+def body_no_wings(size) -> Graphic:
     return compose(pin(bottom_center, beak_feet_belly_eyes(size)),
                    pin(bottom_center, ellipse(size * 2.35, size * 2.6, black)))
 
--- 0001 Developing projects/0000 Python/0000 Penguin_Shape 1 (pytamaro library)/main.py
+++ 0001 Developing projects/0000 Python/0000 Penguin_Shape 1 (pytamaro library)/main.py
@@ -4,7 +4,7 @@ import helper_functions as helper  # defines how the helper_functions.py file is
 
 def penguin_shape(size) -> Graphic:  # the penguin size scale as needed
     return compose(pin(center_right, compose(pin(center, helper.wing(size / 2)),
-                   pin(center_left, helper.body(size)))), helper.wing(size / 2))
+                   pin(center_left, helper.body_no_wings(size)))), helper.wing(size / 2))
 
 
-show_graphic(penguin_shape(561))
+show_graphic(penguin_shape(700))
