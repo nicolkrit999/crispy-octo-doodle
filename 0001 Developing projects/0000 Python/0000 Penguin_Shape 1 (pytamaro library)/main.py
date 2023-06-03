2757b8411ef583d343cc6d5e2df3e16d00dd3b37
nicolk <117823281+nicolkrit999@users.noreply.github.com>
2023-06-03 14:51:28 +0200

    Update main.py

--- 0001 Developing projects/0000 Python/0000 Penguin_Shape 1 (pytamaro library)/main.py
+++ 0001 Developing projects/0000 Python/0000 Penguin_Shape 1 (pytamaro library)/main.py
@@ -2,9 +2,10 @@ from pytamaro import *
 import helper_functions as helper  # defines how the helper_functions.py file is called here
 
 
-def penguin_shape(size) -> Graphic:  # the penguin size scale as needed
+def penguin_shape(size: float) -> Graphic:  # the penguin size scale as needed
+    assert size >= 0
     return compose(pin(center_right, compose(pin(center, helper.wing(size / 2)),
-                   pin(center_left, helper.body_no_wings(size)))), helper.wing(size / 2))
+                                                 pin(center_left, helper.body_no_wings(size)))), helper.wing(size / 2))
 
 
-show_graphic(penguin_shape(700))
+show_graphic(penguin_shape(10000))
