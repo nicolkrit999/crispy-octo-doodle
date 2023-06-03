0bdb338a74e12cd5afdfc41e2b20fda7037260e3
nicolk <117823281+nicolkrit999@users.noreply.github.com>
2023-06-03 17:40:46 +0200

    Pinguino completo con assert su funzione principale

--- 0001 Developing projects/0000 Python/0000 Penguin_Shape 1 (pytamaro library)/main.py
+++ 0001 Developing projects/0000 Python/0000 Penguin_Shape 1 (pytamaro library)/main.py
@@ -2,7 +2,7 @@ from pytamaro import *
 import helper_functions as helper  # defines how the helper_functions.py file is called here
 
 
-def penguin_shape(size: float) -> Graphic:  # the penguin size scale as needed
+def penguin_shape(size: float) -> Graphic:  # size scale as needed. A size over 1000 may take some time to compile
     assert size >= 0  # a graphic with value 0 is possible but not visible
     assert size <= 1.7976931348623157e+308  # maximum float size accepted by python (64-bit double-precision)
     return compose(pin(center_right, compose(pin(center, helper.wing(size / 2)),  # wings size may be changed
@@ -10,4 +10,4 @@ def penguin_shape(size: float) -> Graphic:  # the penguin size scale as needed
                                                  helper.body_no_wings(size)))), helper.wing(size / 2))
 
 
-show_graphic(penguin_shape(431))
+show_graphic(penguin_shape(500))
